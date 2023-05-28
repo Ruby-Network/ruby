336f664180b9ffe5167c45bb49d99ead35e3f9a6
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-28 04:43:48 -0600

    COOL

--- config/puma.rb
+++ config/puma.rb
@@ -13,3 +13,4 @@ else
   end
 end
 preload_app!
+port ENV['PORT'] || 9292
--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'config'
 set :root, File.dirname(__FILE__)
+#Register all settings in config/settings.yml
 register Config
+set :logging, Settings.verboseLogging
 
+get '/' do
+  "Hello World!"
+end
