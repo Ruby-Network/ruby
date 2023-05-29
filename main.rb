3ccdfaa6eabfb02e8d92f179c1c2e83418e329da
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-29 02:54:59 -0600

    Make UV routes look more like middleware

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'colorize'
 require 'rack-proxy'
 require './ruby/utils.rb'
 require './ruby/bare.rb'
+require './ruby/uv.rb'
 
 set :root, File.dirname(__FILE__)
 set :uvPath, File.join(settings.root, 'node_modules', '@titaniumnetwork-dev', 'ultraviolet', 'dist')
@@ -12,21 +13,12 @@ set :views, File.join(settings.root, 'src', 'views')
 register Config
 set :logging, Settings.verboseLogging
 set :show_exceptions, Settings.verboseLogging
+# Setup BareServer reverse proxy, and UV file routes
 use BareServer
+uvPath()
+
 # Other routes
 get '/' do 
   erb :index
-end
-
-get '/uv/*' do
-  #if the file exists :uvPath send it (exclude uv.config.js)
-  if params[:splat][0] == 'uv.config.js'
-    send_file File.join(settings.public_folder, 'js', 'uv', params[:splat][0])
-  elsif File.exists?(File.join(settings.uvPath, params[:splat][0]))
-    send_file File.join(settings.uvPath, params[:splat][0])
-  else
-    #if the file isn't found from their send it from ours
-    send_file File.join(settings.public_folder, 'js', 'uv', params[:splat][0])
-  end
 end
 
---
+++ ruby/uv.rb
@@ -0,0 +1,11 @@
+def uvPath
+  get '/uv/*' do
+    if params[:splat][0] == 'uv.config.js'
+      send_file File.join(settings.public_folder, 'js', 'uv', params[:splat][0])
+    elsif File.exists?(File.join(settings.uvPath, params[:splat][0]))
+      send_file File.join(settings.uvPath, params[:splat][0])
+    else
+      send_file File.join(settings.public_folder, 'js', 'uv', params[:splat][0])
+    end
+  end
+end
