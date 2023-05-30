0e2330395361685fcbc1ba683fc817e6534abe06
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-29 18:38:33 -0600

    Bru

--- ruby/bare.rb
+++ ruby/bare.rb
@@ -2,8 +2,8 @@ class BareServer < Rack::Proxy
    def perform_request(env)
      request = Rack::Request.new(env)
      if request.path.start_with?("/bare")
-       env["HTTP_HOST"] = "localhost:9293"
-       env["PATH_INFO"] = env["PATH_INFO"].gsub("/bare", "")
+       env["HTTP_HOST"] = "localhost:#{Settings.barePort}"
+       env["PATH_INFO"] = request.path.gsub("/bare", "")
        super(env)
      else
        @app.call(env)
