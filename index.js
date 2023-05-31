776c57b3ecd66730640650cc1892db80825f6500
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-31 02:52:59 -0600

    Sass stuff

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ import YAML from 'yaml';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import sass from 'sass';
 import cluster from 'cluster';
+import chalk from 'chalk';
+import createLogger from 'progress-estimator';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 const __dirname = path.resolve();
 const settings = YAML.parse(fs.readFileSync(path.join(__dirname, '/config/settings.yml'), 'utf8'));
 
 let rubyPort = process.argv.find((arg) => arg.startsWith('--ruby-port')).split('=')[1] || 9292;
 let nodePort = process.argv.find((arg) => arg.startsWith('--node-port')).split('=')[1] || 9293;
+const logger = createLogger()
+async function compileSCSS() {
+    fs.readdirSync(path.join(__dirname, '/src/public/sass')).forEach((file) => {
+        if (file.endsWith('.scss')) {
+        let test = sass.compile(path.join(__dirname, `/src/public/sass/${file}`));
+            fs.writeFileSync(path.join(__dirname, `/src/public/css/${file.replace('.scss', '.css')}`), test.css);
+        }
+    });
+}
+await logger(compileSCSS(), 'Compiling SCSS');
 const httpServer = http.createServer();
 const app = express();
 
@@ -40,7 +53,7 @@ httpServer.on('upgrade', (req, socket, head) => {
 });
 
 httpServer.on('listening', () => {
-	console.log(`Server listening on port ${settings.port}, With PID: ${process.pid}`);
+	console.log(chalk.blue(`Server listening on port ${settings.port}, With PID: ${process.pid}`));
 });
 
 httpServer.listen({
