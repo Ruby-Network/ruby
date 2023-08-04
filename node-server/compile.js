385e7d1b443280b3fed15e06447d4343ecf449dd
MotorTruck1221 <tuckerj0606@icloud.com>
2023-08-04 00:56:26 -0600

    Switch to fastify

---
+++ node-server/compile.js
@@ -0,0 +1,19 @@
+import path from 'path'
+import fs from 'fs'
+const __dirname = path.resolve();
+import chalk from 'chalk';
+import createLogger from 'progress-estimator';
+import * as sass from 'sass';
+async function compile() {
+    const logger = createLogger()
+    async function compileSCSS() {
+        fs.readdirSync(path.join(__dirname, '/src/public/sass')).forEach((file) => {
+            if (file.endsWith('.scss')) {
+                let test = sass.compile(path.join(__dirname, `/src/public/sass/${file}`));
+                fs.writeFileSync(path.join(__dirname, `/src/public/css/${file.replace('.scss', '.css')}`), test.css);
+            }
+        });
+    }
+    await logger(compileSCSS(), 'Compiling SCSS');
+}
+export default compile;
---
+++ node-server/server.js
@@ -0,0 +1,48 @@
+import Fastify from 'fastify';
+import fastifyMiddie from '@fastify/middie';
+import fastifyHttpProxy from '@fastify/http-proxy';
+import { fileURLToPath } from 'node:url';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import { createServer } from 'http';
+import fs from 'fs'
+import YAML from 'yaml';
+import path from 'path';
+const __dirname = path.resolve();
+const settings = YAML.parse(fs.readFileSync(path.join(__dirname, '/config/settings.yml'), 'utf8'));
+import chalk from 'chalk';
+import compile from './compile.js';
+let rubyPort = process.argv.find((arg) => arg.startsWith('--ruby-port')).split('=')[1] || 9292;
+let nodePort = process.argv.find((arg) => arg.startsWith('--node-port')).split('=')[1] || 9293;
+
+const bareServer = createBareServer('/bare/');
+console.log(chalk.red('Compiling...'))
+compile();
+
+const bareHandler = (handler, opts) => {
+    return createServer().on('request', (req, res) => {
+        bareServer.shouldRoute(req) ? bareServer.routeRequest(req, res) : handler(req, res);
+    })
+    .on('upgrade', (req, socket, head) => {
+        if ( bareServer.shouldRoute(req) ) {
+            bareServer.routeUpgrade(req, socket, head);
+        }
+    });
+};
+
+const app = Fastify({ logger: false, serverFactory: bareHandler })
+await app 
+    .register(fastifyHttpProxy, {
+        upstream: 'http://localhost:9292',
+        prefix: '/',
+        http2: false,
+        replyOptions: {
+            rewriteRequestHeaders: (originalReq, headers) => {
+                headers['host'] = originalReq.headers['host'];
+                return headers;
+            }
+        }
+    })
+    .register(fastifyMiddie)
+
+app.listen({ port: nodePort });
+console.log(chalk.green(`Server listening on port ${chalk.red(nodePort)}`));
