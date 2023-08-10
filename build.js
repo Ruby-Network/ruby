0ecb5c6ca0a785aa54fca54146ae630e12ebfe27
MotorTruck1221 <tuckerj0606@icloud.com>
2023-08-10 03:36:59 -0600

    Keep Dynamic updated

---
+++ build.js
@@ -0,0 +1,26 @@
+import exec from 'child_process';
+import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+console.log(chalk.green('Setting up Files and folders'));
+if (fs.existsSync('./src/public/js/dynamic/')) {
+    if (fs.existsSync('./src/public/js/dynamic/dynamic.config.js')) {
+        fs.mkdirSync('./tmp');
+        fs.renameSync('./src/public/js/dynamic/dynamic.config.js', './tmp/dynamic.config.js');
+        fs.rmdirSync('./src/public/js/dynamic/', { recursive: true });
+    }
+    else {
+        fs.rmdirSync('./src/public/js/dynamic/', { recursive: true });
+    }
+}
+console.log(chalk.red('Done with Files and folders'));
+console.log(chalk.green('Starting Dynamic build'));
+exec.execSync('npm i', { cwd: './dynamic/', stdio: 'inherit' });
+exec.execSync('npm run build', { cwd: './dynamic/', stdio: 'inherit' });
+console.log(chalk.green('Dynamic build complete'));
+console.log(chalk.magenta('Moving files'));
+fs.renameSync('./dynamic/static/dynamic/', './src/public/js/dynamic/');
+fs.renameSync('./tmp/dynamic.config.js', './src/public/js/dynamic/dynamic.config.js');
+fs.rmdirSync('./tmp');
+console.log(chalk.bgGreen.black('Dynamic build complete'));
