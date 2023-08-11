08d04a158fa2241bdc69b0042390b80323bbc88b
MotorTruck1221 <tuckerj0606@icloud.com>
2023-08-11 00:27:28 -0600

    Start control bar
    Linear: completes: RUBY-19

--- src/public/js/controls.js
+++ src/public/js/controls.js
@@ -7,3 +7,41 @@ function settingsPage(value) {
         document.getElementById('settings-container').classList.add('dnone');
     }
 }
+function refreshPage() {
+    let currentTab = getCurrentTab();
+    let iframe = document.querySelector(`[data-iframe-id="${currentTab}"]`);
+    try {
+        iframe.contentWindow.location.reload();
+    } catch (err) {};
+}
+function previousPage() {
+    let currentTab = getCurrentTab();
+    let iframe = document.querySelector(`[data-iframe-id="${currentTab}"]`);
+    try {
+        iframe.contentWindow.history.back();
+    } catch (err) {};
+}
+function nextPage() {
+    let currentTab = getCurrentTab();
+    let iframe = document.querySelector(`[data-iframe-id="${currentTab}"]`);
+    try {
+        iframe.contentWindow.history.forward();
+    } catch (err) {};
+}
+function popout() {
+    let currentTab = getCurrentTab();
+    let iframe = document.querySelector(`[data-iframe-id="${currentTab}"]`);
+    try {
+        let url = iframe.src;
+        let win = window.open(url, '_blank');
+        win.focus();
+    } catch (err) {};
+}
+function fullscreen() {
+    //this has two options, fullscreen the page, and fullscreen the tab to the window (add later)
+    let currentTab = getCurrentTab();
+    let iframe = document.querySelector(`[data-iframe-id="${currentTab}"]`);
+    try {
+        iframe.contentWindow.document.documentElement.requestFullscreen();
+    } catch (err) {};
+}
