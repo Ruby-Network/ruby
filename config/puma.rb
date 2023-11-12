c108f422e607e6a6434c5586d8e9b5b3afec73f9
MotorTruck1221 <tuckerj0606@icloud.com>
2023-11-12 03:34:48 -0700

    Spawn process even when started singularly

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,8 @@ if OS.windows?
   else 
     system("node node-server/server-dev.js --ruby-port=#{rubyPort} --node-port=9293 &")
   end
-else
+#if processes are able to be forked
+elsif Process.respond_to?(:fork)
   workers Etc.nprocessors
   before_fork do
     puts "Master Process ID: #{Process.pid}".colorize(:green)
@@ -23,6 +24,16 @@ else
     else 
       system("node node-server/server-dev.js --ruby-port=#{rubyPort} --node-port=9293 &")
     end
+  end
+else
+  puts "Server starting in single-threaded mode...".colorize(:red)
+  puts "Starting Server...".colorize(:green)
+  rubyPort = 9292
+  rubyPort = ARGV[ARGV.index('-p') + 1] || ARGV[ARGV.index('--port') + 1] if ARGV.include?('-p') || ARGV.include?('--port')
+  if ENV['RACK_ENV'] == 'production'
+    system("node node-server/server.js --ruby-port=#{rubyPort} --node-port=9293 &")
+  else 
+    system("node node-server/server-dev.js --ruby-port=#{rubyPort} --node-port=9293 &")
   end
 end
 preload_app!
