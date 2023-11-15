ac713e2d66351d52b54fcfe38088ded0481b84b2
MotorTruck1221 <tuckerj0606@icloud.com>
2023-11-15 11:34:00 -0700

    .

--- src/public/js/uv/uv.config.js
+++ src/public/js/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/js/sw/service/uv/',
-    bare: 'http://localhost:4000/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
