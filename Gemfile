a63d33f4e0caa24662910cf691d14d1d7dff523d
MotorTruck1221 <motortruck1221@protonmail.com>
2024-03-14 04:43:35 -0600

    Add support for truffleruby and jruby

--- Gemfile
+++ Gemfile
@@ -15,11 +15,13 @@ gem 'dry-schema'
 gem 'dry-validation'
 gem 'yaml'
 gem 'sequel'
-gem 'pg'
+#gem 'pg'
 gem 'bcrypt'
 gem 'thor'
 gem 'readline'
-gem 'readline-ext'
+#gem 'readline-ext'
+gem "pg", :platforms => [:ruby, :truffleruby]
+gem "pg_jruby", :platform => :jruby
 gem 'httparty'
 gem "rack-reverse-proxy", require: "rack/reverse_proxy"
 group :development do
--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 require 'etc'
 require 'os'
+require 'colorize'
+# get the ruby Implementation
 if OS.windows?
   puts "You are using Windows. Not multi-threading...".colorize(:red)
   puts "Starting Server...".colorize(:green)
@@ -26,12 +28,24 @@ elsif ENV['WP'] != nil
     end
   end
 else
-  workers Etc.nprocessors
-  before_fork do
-    puts "Master Process ID: #{Process.pid}".colorize(:green)
+  if RUBY_ENGINE != 'jruby' && RUBY_ENGINE != 'truffleruby'
+    workers Etc.nprocessors
+    before_fork do
+      puts "Master Process ID: #{Process.pid}".colorize(:green)
+      puts "Starting Server...".colorize(:green)
+      rubyPort = 9292
+      cpuCount = Etc.nprocessors
+      rubyPort = ARGV[ARGV.index('-p') + 1] || ARGV[ARGV.index('--port') + 1] if ARGV.include?('-p') || ARGV.include?('--port')
+      if ENV['RACK_ENV'] == 'production'
+        system("node node-server/server.js --ruby-port=#{rubyPort} --node-port=9293 &")
+      else 
+        system("node node-server/server-dev.js --ruby-port=#{rubyPort} --node-port=9293 &")
+      end
+    end
+  else 
+    puts "You are using JRuby or TruffleRuby. Not using workers...".colorize(:red)
     puts "Starting Server...".colorize(:green)
     rubyPort = 9292
-    cpuCount = Etc.nprocessors
     rubyPort = ARGV[ARGV.index('-p') + 1] || ARGV[ARGV.index('--port') + 1] if ARGV.include?('-p') || ARGV.include?('--port')
     if ENV['RACK_ENV'] == 'production'
       system("node node-server/server.js --ruby-port=#{rubyPort} --node-port=9293 &")
