88569665eb5646491a4db809d0ee5ae5c1dfe54f
MotorTruck1221 <motortruck1221@protonmail.com>
2024-04-08 14:30:21 -0600

    The bare clients...

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -1,11 +1,14 @@
 'serviceWorker' in navigator &&
-    window.addEventListener('load', async function () {
-        await navigator.serviceWorker.register('/sw.js', { scope: '/' }) 
-        //uv SW  
-        await navigator.serviceWorker.register('/js/sw/uv.js', { scope: '/js/sw/service/uv/' })
-        //await navigator.serviceWorker.register('/js/sw/dynamic.js', { scope: '/js/sw/service/dynamic/' })
-        //await setDefaultTransport();
-    });
+    //window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }) 
+    //uv SW  
+    navigator.serviceWorker.register('/js/sw/uv.js', { scope: '/js/sw/service/uv/' }).then(
+        function (registration) {
+            console.log("UV SW Registered!");
+            setDefaultTransport();
+        }).catch(function (error) {
+            console.log("UV SW Registration failed: ", error);
+        });
 function regSW() {
     'serviceWorker' in navigator && 
     navigator.serviceWorker.register('/sw.js', { scope: '/' })
